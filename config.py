571c7d568dc0db3a0ad22653eee4e1009696a297
TGWorkPlace <DramaShip.org@gmail.com>
2026-09-15 09:16:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ import os
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
